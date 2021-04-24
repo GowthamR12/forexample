088bb8833b80a23287fe8c67eee1a6f048b3c23f
Gowtham Raghunathan <gowthamraghunathan@gmail.com>
2021-04-24 14:09:47 +0530

    Interface

--- embstruct.go
+++ embstruct.go
@@ -11,6 +11,22 @@ type secretAgent struct {
 	person
 	ltk bool
 }
+func (s secretAgent) speak(){
+	fmt.Println("I'm",s.first,s.last," secret agent speaks")
+}
+func (s person) speak(){
+	fmt.Println("I'm",s.first,s.last,"Person speaks")
+}
+type human interface{
+	speak()
+}
+func bar(h human){
+	switch h.(type){
+	case person: fmt.Println("Hello person",h.(person).first)
+	case secretAgent:fmt.Println("Hello SA",h.(secretAgent).first)
+	}
+	fmt.Println("hello",h)
+}
 
 func main() {
 	sa := secretAgent{
@@ -28,4 +44,10 @@ func main() {
 	}
 	fmt.Println(sa.person, sa.ltk)
 	fmt.Println(p1.first, p1.last, p1.age)
+
+	sa.speak()
+	p1.speak()
+
+	bar(sa)
+	bar(p1)
 }
